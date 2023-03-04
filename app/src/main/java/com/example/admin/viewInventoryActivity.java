4b020e48c1b607b8e12ad159a7d822d174787ca4
TejaswiniK26 <tejaswinikattukuri@gmail.com>
2023-03-03 21:32:09 -0600

    implemented activity_view_inventory.xml, list_layout.xml, user.java, viewInventoryActivity

---
+++ app/src/main/java/com/example/admin/viewInventoryActivity.java
@@ -0,0 +1,17 @@
+package com.example.admin.inventory;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class viewInventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_inventory);
+
+
+  }
+}
