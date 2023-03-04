bb8d001d91203d2ee83af788250a69e7c2f66ba8
Shiva Vangari <49754508+shiva6427@users.noreply.github.com>
2023-03-03 20:51:28 -0600

    Shiva - I have added the Search Activity and XML
    Shiva - I have added the Search Activity and XML

---
+++ app/src/main/java/com/example/admin/inventory/ScanCodeActivitysearch.java
@@ -0,0 +1,34 @@
+package com.example.admin.inventory;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanCodeActivitysearch extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    int MY_PERMISSIONS_REQUEST_CAMERA = 0;
+
+    ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+        scanItemsActivity.resultsearcheview.setText(result.getText());
+
+        onBackPressed();
+    }
+}
