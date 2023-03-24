8b35e96a5e79c277bb2755dedb15f252a083d1cc
Shiva Vangari <49754508+shiva6427@users.noreply.github.com>
2023-03-23 22:13:23 -0500

    Added Buttons, EditText and TextViews to additemActivity
    Added Buttons, EditText and TextViews to additemActivity - Shiva Vangari

--- app/src/main/java/com/example/admin/inventory/additemActivity.java
+++ app/src/main/java/com/example/admin/inventory/additemActivity.java
@@ -2,13 +2,23 @@ package com.example.admin.inventory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 
 public class additemActivity extends AppCompatActivity {
 
     public static TextView resulttextview;
-
+    private EditText itemname,itemcategory,itemprice;
+    private TextView itembarcode;
+    private FirebaseAuth firebaseAuth;
+    Button scanbutton, additemtodatabase;
+    DatabaseReference databaseReference;
+    DatabaseReference databaseReferencecat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
