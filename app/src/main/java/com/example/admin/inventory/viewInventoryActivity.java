4a77231737515f133c085a58bfa8ae414c5822d4
TejaswiniK26 <tejaswinikattukuri@gmail.com>
2023-03-24 17:59:13 -0500

    Added methods of DataChange for noofitems and totalsum

--- app/src/main/java/com/example/admin/inventory/viewInventoryActivity.java
+++ app/src/main/java/com/example/admin/inventory/viewInventoryActivity.java
@@ -8,10 +8,14 @@ import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Map;
 
@@ -38,7 +42,43 @@ public class viewInventoryActivity extends AppCompatActivity {
         mrecyclerview.setHasFixedSize(true);
         mrecyclerview.setLayoutManager(new LinearLayoutManager(this));
 
+        mdatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    counttotalnoofitem = (int) dataSnapshot.getChildrenCount();
+                    totalnoofitem.setText(Integer.toString(counttotalnoofitem));
+                }else{
+                    totalnoofitem.setText("0");
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        mdatabaseReference.addValueEventListener(new ValueEventListener() {
+         @Override
+         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+             int sum=0;
+             for(DataSnapshot ds : dataSnapshot.getChildren()){
+                Map<String,Object> map = (Map<String, Object>) ds.getValue();
+                Object price = map.get("itemprice");
+                int pValue = Integer.parseInt(String.valueOf(price));
+                sum += pValue;
+                totalnoofsum.setText(String.valueOf(sum));
+
+              }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+ 
+            }
+      });
 
 
   }
