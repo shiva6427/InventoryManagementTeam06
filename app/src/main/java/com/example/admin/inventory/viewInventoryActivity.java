55e79ee06383996e58d1ff2586cc91842067bd8a
TejaswiniK26 <tejaswinikattukuri@gmail.com>
2023-03-24 17:42:07 -0500

    Update viewInventoryActivity.java
    Adding required functionalities to viewInventoryActivity.java

--- app/src/main/java/com/example/admin/inventory/viewInventoryActivity.java
+++ app/src/main/java/com/example/admin/inventory/viewInventoryActivity.java
@@ -3,14 +3,37 @@ package com.example.admin.inventory;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
+
+import java.util.Map;
 
 public class viewInventoryActivity extends AppCompatActivity {
-
+    private FirebaseAuth firebaseAuth;
+    RecyclerView mrecyclerview;
+    DatabaseReference mdatabaseReference;
+   private TextView totalnoofitem, totalnoofsum;
+   private int counttotalnoofitem =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_inventory);
+        totalnoofitem= findViewById(R.id.totalnoitem);
+        totalnoofsum = findViewById(R.id.totalsum);
+        firebaseAuth = FirebaseAuth.getInstance();
+        final FirebaseUser users = firebaseAuth.getCurrentUser();
+        String finaluser=users.getEmail();
+        String resultemail = finaluser.replace(".","");
+        mdatabaseReference = FirebaseDatabase.getInstance().getReference("Users").child(resultemail).child("Items");
+        mrecyclerview = findViewById(R.id.recyclerViews);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        mrecyclerview.setLayoutManager(manager);
+        mrecyclerview.setHasFixedSize(true);
+        mrecyclerview.setLayoutManager(new LinearLayoutManager(this));
+
+
 
 
   }
