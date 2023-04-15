ee4a023a9ef069317890c5b8560650c78d5255ff
Chetana2211 <108295579+Chetana2211@users.noreply.github.com>
2023-04-14 22:01:26 -0500

    Updated ScanCodeActivitydel with added functionalities
    Updated ScanCodeActivitydel with added functionalities

--- app/src/main/java/com/example/admin/inventory/ScanCodeActivitydel.java
+++ app/src/main/java/com/example/admin/inventory/ScanCodeActivitydel.java
@@ -31,5 +31,22 @@ public class ScanCodeActivitydel extends AppCompatActivity implements ZXingScann
 
         onBackPressed();
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+    
 
 }
