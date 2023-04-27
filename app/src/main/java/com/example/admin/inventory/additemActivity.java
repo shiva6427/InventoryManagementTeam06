48520446862bb7b25b1816eb3f737219bfef804e
Shiva Vangari <49754508+shiva6427@users.noreply.github.com>
2023-04-26 22:52:59 -0500

    Made changes to scanItems

--- app/src/main/java/com/example/admin/inventory/additemActivity.java
+++ app/src/main/java/com/example/admin/inventory/additemActivity.java
@@ -12,7 +12,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//import com.example.admin.inventorymanagement.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
